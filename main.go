f414bccc71566da28f339b36a222d18ea9f9f1dc
Roberth Strand <me@robstr.dev>
2024-11-03 09:28:54 +0100

    feat(fiber): set up logger and variable port
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/fehirde/backend/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
-	// Initialize a new Fiber app
+	// Initialize a new Fiber app,
+	// with the logger middleware enabled
 	app := fiber.New()
+	app.Use(logger.New())
 
 	// Register the routes
 	app.Get("/healthz", routes.Healthz)
 
-	// Start the server on port 9020
-	log.Fatal(app.Listen(":9020"))
+	// Start the server on port set by the SERVER_PORT environment variable
+	// If the SERVER_PORT environment variable is not set, default to 9020
+	serverPort := os.Getenv("SERVER_PORT")
+	if serverPort == "" {
+		serverPort = ":9020"
+	}
+	log.Fatal(app.Listen(serverPort))
 }
